01b2e0b92b948124b2c7a157be21e98bd376f2b3
Priyanshu <127112226+priyanxshu999@users.noreply.github.com>
2025-04-29 13:23:53 +0530

    Update app.py

--- Final-proj/app.py
+++ Final-proj/app.py
@@ -23,5 +23,5 @@ app.register_blueprint(routes)
 app.register_blueprint(ip_lookup_bp)
 app.register_blueprint(ssh_bp)  # ✅ Register SSH Console blueprint with prefix
 
-if _name_ == "_main_":
+if _name_ == "__main__":
     app.run(debug=True)
