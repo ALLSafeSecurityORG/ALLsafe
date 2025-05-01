1a54ac0f6e9fcf0c8ea7c3b1d2b811b00331c311
aryan-bhandari <140818067+aryan-bhandari@users.noreply.github.com>
2025-05-01 08:01:14 +0530

    Update detect_lfi.py

--- Final-proj/middleware/detect_lfi.py
+++ Final-proj/middleware/detect_lfi.py
@@ -17,7 +17,7 @@ GEO_API = "http://ip-api.com/json/"
 
 # Email & Discord alert settings
 SENDER_EMAIL = os.getenv("SENDER_EMAIL", "allsafeallsafe612@gmail.com")
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "SuperSecure@123")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "okihsbwykagksikr")
 RECEIVER_EMAILS = os.getenv("RECEIVER_EMAILS", "unknownzero51@gmail.com,aryanbhandari2431@gmail.com").split(",")
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL", "https://discord.com/api/webhooks/123/abc")
 
